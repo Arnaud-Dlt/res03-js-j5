26784d20dd82ddd382030c64cc6a8269c2893c41
Arnaud-Dlt <arnauddeletre@outlook.fr>
2023-01-09 16:04:14 +0000

    17h05

--- Projet_Admin/assets/styles/js/index.js
+++ Projet_Admin/assets/styles/js/index.js
@@ -1,12 +1,26 @@
 let navTitle=document.getElementsByClassName("navTitle");
-
 let submenu=document.getElementsByClassName("submenu");
+let sidemenu=document.getElementById("nav");
+let sidebtn=document.getElementById("side-menu-btn");
+let logo=document.getElementById("logo");
+let sectionGrid=document.getElementById("grid1_3");
 
 
 for (let i = 0; i < submenu.length; i++) {
     
-    navTitle[i].addEventListener("click", function(event) {
-        
-            submenu[i].classList.toggle("close");
-        });
-    }
+    navTitle[i].addEventListener("click", function(event) 
+    {
+        submenu[i].classList.toggle("close");
+    });
+}
+
+
+sidebtn.addEventListener("click", function ()
+{
+    sidemenu.classList.toggle("close");
+    sectionGrid.classList.toggle("sectionGrid");
+    logo.classList.toggle("borderRight");
+})
+
+
+
